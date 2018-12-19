f37e0532afc4b554c4a1bbd5f6da3e9dc0cea6f5
Carlos F S Junior <jradm87@gmail.com>
2018-12-18 23:13:03 -0200

    rules add

---
+++ abstract/add_rule.php
@@ -0,0 +1,23 @@
+<?php
+
+class Rule
+{
+	private $url;
+	private $group;
+	private $ip;
+
+	function add()
+	{
+		echo "Creating rule";
+	}
+
+	function del()
+	{
+		echo "deleting rule";
+	}
+
+	function listRule()
+	{
+		echo "listing rule";
+	}
+}
--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-include_once 'abstract/squid.conf.php';
 echo "Project Squid Manager\n";
-
-$squid = new Squid;
-$squid->port = "3128";
-$squid->start();
 
 ?>
